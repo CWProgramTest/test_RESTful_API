474b416799da91bc2123a39e74104145deec525e
CWProgramTest <cwprogramtest@gmail.com>
2016-09-07 09:42:02 -0700

    Update connect.inc.php

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -2,9 +2,9 @@
 
 $conn_error = 'Failed to connect';
 
-$mysql_host = 'localhost';
-$mysql_user = 'root';
-$mysql_pass = 'password';
+$mysql_host = '';
+$mysql_user = '';
+$mysql_pass = '';
 $mysql_db	= 'Test Database';
 
 if (!mysql_connect($mysql_host, $mysql_user, $mysql_pass) || !mysql_select_db($mysql_db)){
