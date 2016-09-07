04c37e2dd4059e45305fb82e77ce4cdb6224320a
CWProgramTest <cwprogramtest@gmail.com>
2016-09-07 09:46:25 -0700

    Update connect.inc.php

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -2,10 +2,11 @@
 
 $conn_error = 'Failed to connect';
 
+//connect.inc.php data out of date as of 9-6-16
 $mysql_host = '';
 $mysql_user = '';
 $mysql_pass = '';
-$mysql_db	= 'Test Database';
+$mysql_db   = '';
 
 if (!mysql_connect($mysql_host, $mysql_user, $mysql_pass) || !mysql_select_db($mysql_db)){
 	die(mysql_error());
